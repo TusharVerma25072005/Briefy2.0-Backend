285cb3bba732961f3718cc8de4f8a11e12d293bc
Tushar Verma <231210117@nitdelhi.ac.in>
2026-03-12 02:15:10 +0530

    fixing bugs

--- app/api/auth/gmail/callback/route.ts
+++ app/api/auth/gmail/callback/route.ts
@@ -18,10 +18,10 @@ export async function GET(req: NextRequest) {
       process.env.GOOGLE_CLIENT_SECRET,
       process.env.GOOGLE_REDIRECT_URI
     );
-
+    console.log("Received code:", code);
     const { tokens } = await oauth2Client.getToken(code);
     oauth2Client.setCredentials(tokens);
-
+    console.log("OAuth Tokens:", tokens);
     const refreshToken = tokens.refresh_token;
     const accessToken = tokens.access_token;
 
@@ -29,7 +29,7 @@ export async function GET(req: NextRequest) {
       auth: oauth2Client,
       version: "v2",
     });
-
+    console.log("Fetching user info with access token...");
     const userInfo = await oauth2.userinfo.get();
     const userName = userInfo.data.name;
     const userEmail = userInfo.data.email;
