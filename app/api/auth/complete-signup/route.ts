98c14c9cc14df559f9631c1b242ac0251cbaadb9
Tushar Verma <231210117@nitdelhi.ac.in>
2026-03-12 02:03:16 +0530

    still fixing

--- app/api/auth/complete-signup/route.ts
+++ app/api/auth/complete-signup/route.ts
@@ -1,4 +1,6 @@
+export const dynamic = "force-dynamic";
 import { NextRequest, NextResponse } from "next/server";
+
 import prisma from "@/db/prisma";
 import crypto from "crypto";
 export async function POST(req: NextRequest) {
