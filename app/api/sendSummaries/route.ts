fefd36d3967e304ed331db589d3d249c464c6dba
Tushar Verma <231210117@nitdelhi.ac.in>
2026-03-24 01:31:51 +0530

    fixed

--- app/api/sendSummaries/route.ts
+++ app/api/sendSummaries/route.ts
@@ -8,15 +8,7 @@ export async function POST(req: Request){
 
         console.log("Received summaries:", rawEmails);
 
-        const summaries = rawEmails.map((email: any) => ({
-            emailId: email.emailId,
-            summary: `Summary of: ${email.subject}`,
-            priority: "HIGH",
-            embedding: "dummy_embedding",
-            category: "WORK"
-        }));
-
-        return NextResponse.json(summaries, { status: 200 });
+        return NextResponse.json({message : "Sucessfully reciueved"} ,  { status: 200 });
 
     }catch(e){
         return NextResponse.json(
